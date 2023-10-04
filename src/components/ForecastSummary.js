a5a779912dcbff5572502e7d5d24bd581a6b5365
Richard Hallam <richardhallam25@gmail.com>
2023-10-04 18:29:14 +0100

    Implimented openweathermap.org api

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -1,5 +1,25 @@
 import React from "react";
 import forecast from "../data/forecast.json";
+import iconData from "../data/iconData.json";
+
+
+function simplifyWeatherCode(weatherCode){
+    let simplifiedCode = 0;
+    if (weatherCode < 300 && weatherCode >= 200){
+        simplifiedCode = 200;
+    } else if (weatherCode < 400 && weatherCode >= 300){
+        simplifiedCode = 300;
+    }else if (weatherCode < 600 && weatherCode >= 500){
+        simplifiedCode = 500;
+    }else if (weatherCode < 700 && weatherCode >= 600){
+        simplifiedCode = 600;
+    }else if (weatherCode < 800 && weatherCode >= 700){
+        simplifiedCode = 700;
+    }else if (weatherCode < 900 && weatherCode >= 800){
+        simplifiedCode = 800;
+    }
+    return simplifiedCode;
+}
 
 
 function ForecastSummary(props){
@@ -18,8 +38,8 @@ function ForecastSummary(props){
         <div className="forecast-summary_date">
             {formattedDate}
         </div>
-        <div className="forecast-summary_icon">
-            {icon}
+        <div className="forecast-summary_icon" data-testid="forecast-icon">
+            <img src = {iconData[simplifyWeatherCode(icon)]} alt=''/>
         </div>
         <div className="forecast-summary_temperature">
             {temperature.max}
